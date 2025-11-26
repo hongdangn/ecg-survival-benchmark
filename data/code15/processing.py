34a047103ede34fe9c3bf9ab1e61cd1c4f2944b7
hongdangn <ddangdangnh@gmail.com>
2025-11-26 12:09:38 +0700

    add requirements/test/download

--- data/code15/processing.py
+++ data/code15/processing.py
@@ -16,18 +16,19 @@ from torch import manual_seed
 from torch.utils.data import random_split
 import pandas as pd
 
+manual_seed(12345)
+
 # move to correct folder
-fold_path = os.path.join(os.path.dirname(os.getcwd()), 'RAW_DATA', 'Code15')
+fold_path = os.path.join(os.path.dirname(os.getcwd()), 'data', 'code15')
 os.chdir(fold_path)
 
-
-#%% Data directories
-Code15_ECG_Directory = os.path.join(os.getcwd(),'ECG')
+Code15_ECG_Directory = os.path.join(os.getcwd(),'ecg')
 Code15_exams_loc = os.path.join(os.getcwd(),'exams.csv')  
 pd_path = os.path.join(os.getcwd(), 'Labels_Code15_mort_032025_pd_8020.csv') 
 
 # %% Adjust labels
 labels = pd.read_csv(Code15_exams_loc)
+labels = labels.loc[labels["trace_file"] == "exams_part0.hdf5"].copy().reset_index()
 
 # rename columns
 labels.rename(columns={'exam_id': 'SID'}, inplace=True)
@@ -39,17 +40,12 @@ labels.rename(columns={'patient_id': 'PID'}, inplace=True)
 
 # adjust tte_mortality to min of 1/365
 labels.loc[labels['Mort_TTE']<1/365,'Mort_TTE'] = 1/365
-
-
-# now split into train/test based off unique PID with TTE not NaN
 legit_PID = labels.loc[np.isnan(labels['Mort_TTE'])==False]['PID'].to_numpy() # this matches np.unique(PIDs)
 
 Te_Count = int(len(legit_PID) * 0.2)
 Tr_Count = len(legit_PID) - Te_Count
-manual_seed(12345)
 PID_Index_Train, PID_Index_Test = random_split(range(len(legit_PID)), [Tr_Count, Te_Count])   
 
-# assign each PID with !isnan(TTE) to 'tr' or 'te'
 PID_labels = {}
 for i in PID_Index_Train:
     temp_PID = legit_PID[i]
@@ -68,45 +64,30 @@ for PID,TTE in zip(labels['PID'],labels['Mort_TTE']):
         
 labels['Test_Train_split_12345'] = Test_Train_split_12345
 
-
-# %% Compact the ECG files
-# Grab data placed in 'ECG' folder
 EIDs = []
 ECGs = []
-for f in os.listdir(Code15_ECG_Directory): # load all hdf5
+for f in os.listdir(Code15_ECG_Directory): 
     print(f)
     with h5py.File(os.path.join(Code15_ECG_Directory,f), "r") as h:
         print("Keys: %s" % h.keys())
-        # exam_id = h['exam_id'][()]
-        # tracings = h['tracings'][()]
-        # EIDs.append(exam_id[:-1])        # the last one is just '0'
-        # ECGs.append(tracings[:-1])      # ... so cut it
         EIDs.append(h['exam_id'][:-1])
         ECGs.append(h['tracings'][:-1,648:-648,:].astype(np.float32)) # keep the middle 7 seconds @ 400Hz = 2800 samples
         
 EIDs = np.hstack([k for k in EIDs])
 ECGs = np.vstack([k for k in ECGs])
-# del exam_id
-# del tracings
 
-# %% re-order the ECGs to match the .csv
-
-# from the ECG, tag each SID with a number
 ECG_SID_label_dict = {}
 for i,SID in enumerate(EIDs):
     ECG_SID_label_dict[SID] = i
     
-# then, parse label SIDs, making a list of which label row we want
 reorder_arr = []
 for SID in labels["SID"]:
-    if (SID in ECG_SID_label_dict.keys()): # sometimes we do this on a debug dataset
+    if (SID in ECG_SID_label_dict.keys()): 
         reorder_arr.append(ECG_SID_label_dict[SID])
     
 EIDs = EIDs[reorder_arr]
 ECGs = ECGs[reorder_arr]
 
-
-# %% save out stacked ECG
 with h5py.File('Code15_ECG.hdf5', "w") as f:
     f.create_dataset('ECG',       data = ECGs, chunks = (1,2800,12),  compression="gzip")
     f.create_dataset('SID',       data = EIDs)
---
+++ download_data.sh
@@ -0,0 +1,3 @@
+wget -O exams_part0.zip https://zenodo.org/records/4916206/files/exams_part0.zip
+unzip exams_part0.zip
+rm exams_part0.zip
